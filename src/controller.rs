6f1e5d4ae71b168bce0e0398219e652f519788e0
Henry de Valence <hdevalence@hdevalence.ca>
2021-05-12 23:08:50 -0700

    Add goal-oriented charging.
    This helps prevent some problems by allowing arbitrary goals (currently only
    two) and picking whichever one has the most required charging.  Not doing this
    can lead to a situation where a nearer-term goal to a lower target prevents
    charging for a later goal with a higher target.

--- src/controller.rs
+++ src/controller.rs
@@ -5,6 +5,36 @@ use sgip_signal::{Forecast, Moer};
 use super::config;
 use crate::{DurationExt, ForecastExt, History};
 
+struct Goal<'c> {
+    time: DateTime<Utc>,
+    charge: f64,
+    config: &'c config::Charging,
+}
+
+impl<'c> std::fmt::Debug for Goal<'c> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Goal")
+            .field("time", &self.time)
+            .field("charge", &self.charge)
+            .finish()
+    }
+}
+
+impl<'c> Goal<'c> {
+    fn available_charging_hours(&self, now: DateTime<Utc>) -> f64 {
+        self.config
+            .allowed_times_during(now..self.time)
+            .map(|range| (range.end - range.start).num_hours_f64())
+            .sum()
+    }
+
+    fn required_charging_proportion(&self, now: DateTime<Utc>, present_soc: f64) -> f64 {
+        let charge_kwh = (self.charge - present_soc) * self.config.capacity_kwh;
+        let charge_hours = charge_kwh / self.config.charge_rate_kw;
+        charge_hours / self.available_charging_hours(now)
+    }
+}
+
 impl config::Charging {
     /// Returns whether to charge, and the emissions limit used to make that decision.
     pub fn can_charge(
@@ -26,39 +56,51 @@ impl config::Charging {
             return (false, -1);
         }
 
-        let local_time = now.with_timezone(&Pacific).time();
-        // Determine whether our goal is base charging or flex charging.
-        let below_base_charge = soc <= self.base_charge;
-        let before_base_charge_by = local_time < self.base_charge_by;
-
-        let (target_time, target_charge) = if below_base_charge && before_base_charge_by {
-            (
-                now.with_timezone(&Pacific)
+        let goals = vec![
+            // Base charging: hit a base charge level at a fixed time each day.
+            Goal {
+                time: now
+                    .with_timezone(&Pacific)
                     .date()
                     .and_time(self.base_charge_by)
                     .unwrap()
                     .with_timezone(&Utc),
-                self.base_charge,
-            )
-        } else {
-            (end, self.max_charge)
-        };
+                charge: self.base_charge,
+                config: &self,
+            },
+            // Flex charging: aim to complete the rest of the charging a fixed
+            // time from now, whatever now is.
+            Goal {
+                time: end,
+                charge: self.max_charge,
+                config: &self,
+            },
+        ];
+        tracing::info!(?goals);
 
-        let available_charging_hours: f64 = self
-            .allowed_times_during(now..target_time)
-            .map(|range| (range.end - range.start).num_hours_f64())
-            .sum();
+        // Only retain future goals
+        let mut goals = goals
+            .into_iter()
+            .filter(|goal| goal.time > now)
+            .collect::<Vec<Goal>>();
 
-        let charge_kwh = (target_charge - soc) * self.capacity_kwh;
-        let charge_hours = charge_kwh / self.charge_rate_kw;
-        let charging_time_proportion = charge_hours / available_charging_hours;
+        // Choose the goal with the largest required charging proportion.
+        goals.sort_by(|a, b| {
+            let a_req = a.required_charging_proportion(now, soc);
+            let b_req = b.required_charging_proportion(now, soc);
+
+            a_req.partial_cmp(&b_req).unwrap()
+        });
+        let goal = goals.pop().expect("must have at least one goal");
+        tracing::info!(?goal);
+
+        let available_charging_hours = goal.available_charging_hours(now);
+        let required_charging_proportion = goal.required_charging_proportion(now, soc);
 
         metrics::gauge!("soc", soc);
         metrics::gauge!("available_charging_hours", available_charging_hours);
-        metrics::gauge!("charge_kwh", charge_kwh);
-        metrics::gauge!("charge_hours", charge_hours);
-        metrics::gauge!("charging_time_proportion", charging_time_proportion);
-        metrics::gauge!("target_charge", target_charge);
+        metrics::gauge!("required_charging_proportion", required_charging_proportion);
+        metrics::gauge!("target_charge", goal.charge);
 
         // The SGIP forecasts often get the curve right but offset up or down,
         // which biases the forecast emissions data, so combine the forecast
@@ -66,7 +108,7 @@ impl config::Charging {
         let lookback = self
             .allowed_times_during((now - Duration::hours(2 * self.flex_charge_hours))..now)
             .collect();
-        let lookahead = self.allowed_times_during(now..target_time).collect();
+        let lookahead = self.allowed_times_during(now..goal.time).collect();
 
         let mut emissions = history.histogram_over(lookback) + forecast.histogram_over(lookahead);
 
@@ -77,15 +119,12 @@ impl config::Charging {
         let current_rate = (current.rate * 1000.) as u64;
         emissions += current_rate;
 
-        let emissions_limit = emissions.value_at_quantile(charging_time_proportion);
+        let emissions_limit = emissions.value_at_quantile(required_charging_proportion);
         tracing::debug!(
-            ?target_time,
+            ?goal.time,
+            ?goal.charge,
             ?soc,
-            ?target_charge,
-            ?charge_kwh,
-            ?charge_hours,
-            ?available_charging_hours,
-            ?charging_time_proportion,
+            ?required_charging_proportion,
             ?emissions_limit,
             ?current_rate,
             can_charge = (current_rate <= emissions_limit),
@@ -101,7 +140,7 @@ impl config::Charging {
         metrics::gauge!("emissions_max", emissions_quantile(1.00));
         metrics::gauge!(
             "emissions_limit",
-            emissions_quantile(charging_time_proportion)
+            emissions_quantile(required_charging_proportion)
         );
 
         (current_rate <= emissions_limit, emissions_limit as i64)
